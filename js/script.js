3d5ce8d29efd96e248077509905c4717ee73a944
danathabtamu <142098362+danathabtamu@users.noreply.github.com>
2026-06-01 08:04:18 +0300

    just start the javascript

--- js/script.js
+++ js/script.js
@@ -1,44 +1,10 @@
-// function List(id,name,task){
-// 	this.id = id;
-// 	this.name = name;
-// 	this.task = task;
-// }
+let add_list_btn = $("#add_list")
+let list_name_taker = $(".list_name_taker")
+let cancel_add_list = $("#cancel_btn")
 
-// let list1 = new List(1,"weekly",[{id:1,name:"finish a project",progress:"unfinished"}])
-
-// let lists = [{}]
-
-
-// lists[0].id = 1
-// lists[0].name = "monthly"
-// lists[0].task = {
-// 	id:1,
-// 	name:"take a shower",
-// 	progress:"unfinished"
-// }
-// lists[1].id = 2
-// lists[1].name = "weekly"
-// lists[1].task = {
-// 	id:2,
-// 	name:"finish project",
-// 	progress:"unfinished"
-// }
-
-let lists = [
-	{
-		id:1,
-		name:"daily",
-		task:[{
-			id,
-			taskName
-		}]
-	}
-]
-
-let addToLists = (id,name,task)=>{
-	lists.push({id:id,name:name,task,task})
-}
-
-addToLists(2,"weekly",{id:1,taskName:"study"})
-
-console.log(lists)
+add_list_btn.on("click",()=>{
+	list_name_taker.removeClass("hidden")
+})
+cancel_add_list.on("click",()=>{
+	list_name_taker.addClass("hidden")
+})
