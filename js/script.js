55db5000deabb146204f66a4e1c168b7e0980bd0
danathabtamu <142098362+danathabtamu@users.noreply.github.com>
2026-06-01 10:23:57 +0300

    task taking on progress

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 let add_list_btn = $("#add_list")
+let add_task_btn = $("#add_task")
 let list_name_taker = $(".list_name_taker")
 let cancel_add_list = $("#cancel_btn")
 let push_add_list_btn = $("#add_list_btn")
 let list_name_input = $("#list_name")
 let listItems = $(".listItems")
+let each = $(".lists")
 
 let lists =[
 	{
@@ -19,15 +21,32 @@ let renderListName = (listName)=>{
 add_list_btn.on("click",()=>{
 	list_name_taker.removeClass("hidden")
 })
+add_task_btn.on("click",()=>{
+	// list_name_taker.removeClass("hidden")
+})
 cancel_add_list.on("click",()=>{
 	list_name_taker.addClass("hidden")
 })
 push_add_list_btn.on("click",()=>{
 	let name = list_name_input.val()
+	if (name.trim().length === 0) {
+		alert("pls insert the name")
+		return
+} 
 	lists.push({name:name,task:[]})
 	list_name_taker.addClass("hidden")
 	listItems.html(" ")
 	lists.forEach((eachList)=>{
 		renderListName(eachList.name)
 	})
+	list_name_input.val(" ")
+	console.log(listItems) 
 })
+
+
+each.on("click",(e)=>{
+	let clicked_list = $(this).val()
+	console.log("lists clicked")
+	console.log(clicked_list)
+})
+
